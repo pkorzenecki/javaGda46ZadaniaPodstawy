864652eebd43a70dc0ddc759586ae04882c08b40
pkorzenecki <pkorzenecki@gmail.com>
2020-09-20 11:29:02 +0200

    Koniec pierwszego bloku day2

--- src/basic/java/day2/Characters.java
+++ src/basic/java/day2/Characters.java
@@ -1,2 +1,21 @@
-package basic.java.day2;public class Characters {
+package basic.java.day2;
+
+public class Characters {
+    public static void main(String[] args) {
+        System.out.println(getLastCharacterV1("domek"));
+
+        System.out.println(getLastCharacterV2("domek"));
+
+    }
+
+    private static char getLastCharacterV1(String input) {
+        int lastPosition = input.length() -1;
+        return input.charAt(lastPosition);
+
+    }
+
+    private static String getLastCharacterV2(String input) {
+        int lastPosition = input.length() -1;
+        return input.substring(lastPosition);
+    }
 }
--- src/basic/java/day2/WordsExerc.java
+++ src/basic/java/day2/WordsExerc.java
@@ -1,2 +1,20 @@
-package basic.java.day2;public class WordsExerc {
+package basic.java.day2;
+
+public class WordsExerc {
+    public static void main(String[] args) {
+
+        System.out.println(ifWordContainsPrefix("programowanie", "pro"));
+        System.out.println(ifWordContainsSuffix("programowanie", "nie"));
+
+        System.out.println(ifWordContainsPrefix("programowanie", "pro1"));
+        System.out.println(ifWordContainsSuffix("programowanie", "nie1"));
+    }
+
+    private static boolean ifWordContainsPrefix(String input, String prefix) {
+        return input.startsWith(prefix);
+    }
+    private static boolean ifWordContainsSuffix(String input, String suffix) {
+        return input.endsWith(suffix);
+
+    }
 }
